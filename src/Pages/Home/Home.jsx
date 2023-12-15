396fc4194d6860500da893e687065d56597c3faa
chand-stack <chand.stack@gmail.com>
2023-12-15 20:53:39 +0600

    Chore: home page added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,35 @@
 import { Link } from "react-router-dom";
-
+import img from "../../assets/MainImage.png";
 const Home = () => {
   return (
     <div>
-      <h1>this is home page</h1>
-      <Link to="/login" className="btn">
-        Login
-      </Link>
-      <Link to="/register" className="btn">
-        Register
-      </Link>
+      <div className="hero min-h-screen bg-[#F2D335]">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={img} />
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <div className="flex flex-row gap-4">
+              <Link
+                to="/register"
+                className="btn bg-[#F2D335] text-black md:text-xl btn-outline"
+              >
+                Sign In
+              </Link>
+              <Link
+                to="/login"
+                className="btn bg-[#F2D335] text-black md:text-xl btn-outline"
+              >
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
